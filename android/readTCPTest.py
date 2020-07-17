5092be4e4843b857881b3be8f5c2b882ad928a9f
Sanjay Seshan <sanjay.seshan@outlook.com>
2020-07-17 12:41:01 -0400

    changes

---
+++ android/readTCPTest.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python3
+import socket
+#import serial
+import time
+import sys
+import numpy as np
+import keras
+from sklearn.metrics import confusion_matrix, precision_score
+import pandas as pd
+import numpy as np
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("localhost", 8080))
+
+gesturesC = {
+    "relax":1,
+    "extend":2,
+    "fist":3,
+    "one":4,
+        "transition":5,
+        "generic":-1
+}
+
+def getSerial():
+	return s.recv(1).decode()
+
+def getSerialBuf():
+        buf = ""
+        while len(buf.split(",")) != 5:
+                buf = ""
+                newData = ""
+                while not ("\r\n" in buf):
+                        newData = getSerial()
+                        buf = buf + newData
+                        #print(repr(newData))
+                buf = buf.split("\r\n")[0]
+#                print(len(buf.split(",")))
+#                print(repr(buf))
+        return buf
+
+        
+port = sys.argv[1]
+fileOut = sys.argv[2] + "testML"
+datafile = fileOut
+saveFile = sys.argv[2] + 'modelSave2'
+
+f = open(fileOut, "w")
+fx = open(fileOut+"X.csv", "w")
+fy = open(fileOut+"Y.csv", "w")
+
+#ser = serial.Serial(port, 9600, timeout=1)
+
+def getData(timeout, label):
+#    n = 0
+#    ti = time.time()
+#    while time.time()-ti<timeout:
+    #vthreei = pr[4]
+    #tileLast=time.time()
+    lastDelta=0
+    timeI=time.time()
+    reading = getSerialBuf().split(",")
+    pr = [0,0,0,0,0,0,0,"",0]
+    pr[0] = time.time()-timeI
+    pr[1] = time.time()-trel
+    pr[2] = int(reading[0])
+    pr[3] = int(reading[1])
+    pr[4] = int(reading[2])
+    pr[5] = int(reading[3])
+    pr[6] = int(reading[4])
+    pr[7] = label
+    pr[8] = gesturesC[label]
+    #delta = pr[4] - vthreei
+    #dprdt= (delta-lastDelta)/(1) #~0.01 s/loop
+    #tileLast=time.time()
+    #lastDelta=delta
+    for i in pr:
+        #print str(i)+",",
+        f.write(str(i)+",")
+    print(pr)
+    fx.write(str(pr[2])+","+str(pr[3])+","+str(pr[4])+","+str(pr[5])+","+str(pr[6])+"\n")
+    fy.write(str(pr[8])+"\n")
+    #if dprdt == 0:
+    #    vthreei = pr[4]
+    #print ""
+    f.write("\n")
+
+
+model = keras.models.load_model(saveFile)
+gestures = {
+    1: "relax",
+    2: "extend",
+    3: "fist",
+    4: "one",
+    5: "transition"
+}
+def predictML():
+    getData(0,"none")
+    x_data = pd.read_csv(datafile+"X.csv",names=['s1','s2','s3','s4','s5'])
+    x_test = x_data[-500:]
+    y_pred = model.predict(x_test)
+    rounded = [np.argmax(x) for x in y_pred]
+    print("Gesture: "+rounded[-1]+"; "+gestures[rounded[-1]])
+
+
+print("delta Time, Unix Time, pr1, pr2, pr3, pr4, pr5, label")
+f.write("delta Time, Unix Time, pr1, pr2, pr3, pr4, pr5, label\n")
+trel = time.time()
+print(trel)
+
+continueQ = input("Start? [Y/n]: ")
+if continueQ == "y" or continueQ == "" or continueQ == "Y":
+    while True:
+        predictML()
+elif continueQ == "n" or continueQ == "N":
+    continueQ = "n"
+    
+f.close()
--- android/readTCPTrain.py
+++ android/readTCPTrain.py
@@ -3,6 +3,19 @@ import socket
 #import serial
 import time
 import sys
+import numpy as np
+import keras
+from keras.datasets import imdb
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+from sklearn.metrics import confusion_matrix, precision_score
+from sklearn.model_selection import train_test_split
+from keras.layers import Dense,Dropout
+from keras.regularizers import l2
+import matplotlib.pyplot as plt
+import pandas as pd
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("localhost", 8080))
@@ -12,8 +25,7 @@ gesturesC = {
     "extend":2,
     "fist":3,
     "one":4,
-        "transition":5,
-        "generic":-1
+    "transition":5
 }
 
 def getSerial():
@@ -90,19 +102,50 @@ def getData(timeout, label):
 
 #gestures = ["fist", "extend", "one", "two", "three", "four", "five", "spiderman", "vulcan", "index in", "middle in", "ring in", "little in", "thumb in", "lift up", "lift down"]
 
-gestures=["generic"]
+gestures=["relax","transition","extend","transition","relax"]
 
-print("delta Time, Unix Time, pr1, pr2, pr3, pr4, pr5, label")
-f.write("delta Time, Unix Time, pr1, pr2, pr3, pr4, pr5, label\n")
+print("delta Time, Unix Time, pr1, pr2, pr3, pr4, pr5, label, numLabel")
+f.write("delta Time, Unix Time, pr1, pr2, pr3, pr4, pr5, label, numLabel\n")
 trel = time.time()
 print(trel)
 for gesture in gestures:
     continueQ = input("Do "+gesture+"? [Y/n]: ")
     if continueQ == "y" or continueQ == "" or continueQ == "Y":
-        getData(99999999999999999999999999999999999, gesture)
+        getData(5, gesture)
 #            while True:
 #                    print(getSerialBuf().split(","))
     elif continueQ == "n" or continueQ == "N":
         continueQ = "n"
     
 f.close()
+
+datafile = fileOut
+
+x_data = pd.read_csv(datafile+"X.csv",names=['s1','s2','s3','s4','s5'])
+y_dataR = np.genfromtxt(datafile+"Y.csv",delimiter=',',dtype=int)
+num_classes = np.max(y_dataR) + 1
+y_data = keras.utils.to_categorical(y_dataR, num_classes)
+#x_train, x_test, y_train, y_test = train_test_split(x_data,y_data, test_size=0.20, random_state=0)
+x_train = x_data
+x_test = x_data[880:]
+y_train = y_data
+y_test = y_data[880:]
+x_train.shape,y_train.shape,x_test.shape,y_test.shape
+
+#defifne a sequentail Model
+model = Sequential()
+#Hidden Layer-1
+model.add(Dense(100,activation='relu',input_dim=5,kernel_regularizer=l2(0.01)))
+model.add(Dropout(0.3, noise_shape=None, seed=None))
+
+#Hidden Layer-2
+model.add(Dense(100,activation = 'relu',kernel_regularizer=l2(0.01)))
+model.add(Dropout(0.3, noise_shape=None, seed=None))
+
+#Output layer
+model.add(Dense(6,activation='sigmoid'))
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+model_output = model.fit(x_train,y_train,epochs=150,batch_size=20,verbose=1,validation_data=(x_test,y_test),)
+model.save(fileOut+'modelSave2')
+model.summary()
+
